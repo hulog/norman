e83606f995f9816bc7074848d7d123fd2ca60f4b
NormanHu <450382690@qq.com>
2016-11-02 09:50:54 +0800

    newest

--- src/main/java/msgrelationship/norman/sendInterf.java
+++ src/main/java/msgrelationship/norman/sendInterf.java
@@ -2,49 +2,89 @@ package msgrelationship.norman;
 
 public class sendInterf {
 
-    // 账号
-    private String client_id = "hl";
-    // 密码
-    private String client_password = "000000";
     // 0群组，1个人
-    private String send_type = "1";
-    // 发送对象ID， 群组群号/个人编号
-    private String send_id = "2";
-    // 发送方式，1表示微信，2表示邮箱，3表示短信，4表示全部
-    private String send_way = "1";
-    // 信息内容
-    private String message;
+    // send_type
+    private final static String toPerson = "1";
+    private final static String toGoup = "0";
 
-//    private InterfaceTest it = new InterfaceTest();
+    // 发送对象ID， 群组群号/个人编号
+    // send_id
+    private final static String PID = "2";
+    private final static String GID = "1";
+
+    // 发送方式，1表示微信，2表示邮箱，3表示短信，4表示全部
+    // send_way
+    private final static String viaWX = "1";
+    private final static String viaEmail = "2";
+    private final static String viaSms = "3";
+    private final static String viaAll = "4";
+
+    // 信息内容
+    private final static String message = "Congratulations,中英文测试正常";
+
+    // private InterfaceTest it = new InterfaceTest();
     private InterfaceTest it;
-    
-    public sendInterf(){
+
+    public sendInterf() {
         this.it = new InterfaceTest();
-        this.message = "Congratulations,中英文测试正常";
     }
+
     public final void send_P_Wx() {
-        
-        it.msgSend(client_id, client_password, send_id, send_type, send_way, message);
+        if (it.msgSend(PID, toPerson, viaWX, message))
+            syso("success");
+        else
+            syso("failed");
     }
 
     public final void send_P_Em() {
-
+        if (it.msgSend(PID, toPerson, viaEmail, message))
+            syso("success");
+        else
+            syso("failed");
     }
 
     public final void send_P_Sms() {
+        if (it.msgSend(PID, toPerson, viaSms, message))
+            syso("success");
+        else
+            syso("failed");
+    }
 
+    public final void send_P_All() {
+        if (it.msgSend(PID, toPerson, viaAll, message))
+            syso("success");
+        else
+            syso("failed");
     }
 
     public final void send_G_Wx() {
+        if (it.msgSend(GID, toGoup, viaWX, message))
+            syso("success");
+        else
+            syso("failed");
 
     }
 
     public final void send_G_Em() {
+        if (it.msgSend(GID, toGoup, viaEmail, message))
+            syso("success");
+        else
+            syso("failed");
 
     }
 
     public final void send_G_Sms() {
+        if (it.msgSend(GID, toGoup, viaSms, message))
+            syso("success");
+        else
+            syso("failed");
+    }
 
+    public final void send_G_All() {
+        if (it.msgSend(GID, toGoup, viaAll, message))
+            syso("success");
+        else
+            syso("failed");
     }
 
     public final void send_P_search() {
@@ -55,4 +95,7 @@ public class sendInterf {
 
     }
 
+    private void syso(String str) {
+        System.out.println(str);
+    }
 }
